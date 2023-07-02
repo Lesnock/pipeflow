0a1d0b9ab0d7f3b71f1822e4f1f75217208ad7d8
Caio Lesnock <caio.dev@outlook.com>
2023-07-01 23:45:38 -0300

    Add tests for spy callback

--- src/Pipe.ts
+++ src/Pipe.ts
@@ -3,11 +3,34 @@ export type PipeCallback = (data?: unknown) => unknown
 export class Pipe {
     callback: PipeCallback
 
-    constructor(callback: PipeCallback) {
+    earlierData: unknown
+
+    constructor(callback: PipeCallback, earlierData?: unknown) {
         this.callback = callback
+        this.earlierData = earlierData
     }
 
-    get() {
-        return this.callback()
+    /**
+     * Executes the callback function and returns the result
+     * This method "breaks" the pipeflow
+     */
+    get(): unknown {
+        return this.callback(this.earlierData)
+    }
+
+    /**
+     * Executes the callback function but do not returns anything
+     * This method "breaks" the pipeflow
+     */
+    finish() {
+        this.callback(this.earlierData)
+    }
+
+    /**
+     * Executes the callback function and send it to the next pipe.
+     * In practice, creates a new Pipe object.
+     */
+    pipe(callback: PipeCallback): Pipe {
+        return new Pipe(callback, this.get())
     }
 }
--- test/Pipe.spec.ts
+++ test/Pipe.spec.ts
@@ -1,8 +1,27 @@
 import { pipe, Pipe } from '../src'
 import { expect } from 'chai'
+import { assert, spy } from 'sinon'
 
 describe('Pipe', () => {
     it('returns a Pipe object', () => {
         expect(pipe(() => '')).to.instanceOf(Pipe)
+    })
+
+    it('returns callback result on calling get', () => {
+        const result = pipe(() => 'callbackResult').get()
+        expect(result).to.be.equal('callbackResult')
+    })
+
+    it('not returns callback result on calling finish', () => {
+        const result = pipe(() => 'callbackResult').finish()
+        expect(result).to.be.undefined
+    })
+
+    it('should receive callback result of first pipe', () => {
+        const spyCall = spy()
+        pipe(() => 'myArgs')
+            .pipe(spyCall)
+            .get()
+        assert.calledWith(spyCall, 'myArgs')
     })
 })
